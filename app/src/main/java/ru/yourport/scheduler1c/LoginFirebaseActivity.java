ff073096a3f442b668e3543425dec1f40386b0c6
Dmitriy33g <Dmitriy33g@gmail.com>
2019-12-23 18:09:39 +0300

    27

---
+++ app/src/main/java/ru/yourport/scheduler1c/LoginFirebaseActivity.java
@@ -0,0 +1,14 @@
+package ru.yourport.scheduler1c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginFirebaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginfirebase);
+    }
+}
